2a336e86f32a4573720210d865df2b6d6827d04c
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-09 16:53:02 -0400

    Get user input

--- QuickSort.java
+++ QuickSort.java
@@ -1,1 +1,17 @@
-// Test for repo
+// Take user input for QuickSort
+
+import java.util.Scanner;
+
+public class QuickSort{
+    public static void main(String[]args){
+
+       Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many values would you like to enter?");
+        int n = sc.nextInt();
+
+        int arr [] = new int [n];
+        System.out.println("Enter the " + n + " values with spaces between");
+
+    }
+}
