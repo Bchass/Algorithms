90ab8171caff442511e12167b21031d7bdab4cf8
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-10 22:01:15 -0400

    Added methods for finding pivot

--- QuickSort.java
+++ QuickSort.java
@@ -14,6 +14,7 @@ public class QuickSort {
             int n = sc.nextInt();
 
             int arr[] = new int[n];
+            QS(arr, 0, arr.length - 1);
             System.out.println("Enter the " + n + " values with spaces between");
 
             for (int i = 0; i < arr.length; i++) {
@@ -41,5 +42,34 @@ public class QuickSort {
             System.out.print(arr[i] + " ");
         }
         return true;
+    }
+    // Call upon pivot method
+    public static void QS(int arr [], int start, int end){
+        if(start < end){
+            int partitionIndex = partition(arr, start, end);
+
+            QS(arr, start, partitionIndex - 1);
+            QS(arr, partitionIndex + 1, end);
+        }
+    }
+    // Method to find pivot
+    private static int partition(int arr [], int start, int end){
+        int pivot = arr[end];
+        int i = (start - 1);
+
+        for(int x = start; x < end; x++){
+            if(arr[x] <= pivot){
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[x];
+                arr[x] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = temp;
+
+        return i + 1;
     }
 }
