000a6e1b5a30777c6c7c08af471ec5a2b1c5c7ec
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-10 20:36:55 -0400

    Custom try catch statement, format code

--- QuickSort.java
+++ QuickSort.java
@@ -1,35 +1,43 @@
+
 // Take user input for QuickSort
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class QuickSort{
-    public static void main(String[]args){
+public class QuickSort {
+    public static void main(String[] args) {
 
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("How many values would you like to enter?");
-        int n = sc.nextInt();
+        try {
+            System.out.println("How many values would you like to enter?");
+            int n = sc.nextInt();
 
-        int arr [] = new int [n];
-        System.out.println("Enter the " + n + " values with spaces between");
+            int arr[] = new int[n];
+            System.out.println("Enter the " + n + " values with spaces between");
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("The values you have entered:");
+            System.out.println("= " + printArr(arr));
+
+            sc.close();
+
+            // custom try catch statement
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Input must contain only values!");
         }
-
-        System.out.println("The values you have entered:");
-        System.out.println("= " + printArr(arr));
-
-        sc.close();
     }
 
     // Method to print user inputs values to an array
-    // if true numbers will print -- > array 
-    public static boolean printArr(int [] arr){
+    // if true numbers will print -- > array
+    public static boolean printArr(int[] arr) {
 
         int x = arr.length;
 
-        for(int i = 0; i < x; i++){
+        for (int i = 0; i < x; i++) {
             System.out.print(arr[i] + " ");
         }
         return true;
