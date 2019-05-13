5684332ab7e1ce80fae8d8d31377830bc9230256
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-13 17:51:46 -0400

    First commit

---
+++ SelectionSort.java
@@ -0,0 +1,12 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int x = arr.length;
+        for (int i = 0; i < x; i++) {
+            // Index of min
+            int min = 1;
+
+        }
+
+    }
+}
