d16983dddd067020d29d0fc84f5a7f6763eb46cd
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-13 20:18:33 -0400

    For loop to find min

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,10 +1,16 @@
 public class SelectionSort {
     public static void main(String[] args) {
-
+        // move boundary within array
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             // Index of min
             int min = i;
+            // Find min within the array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
 
         }
 
