8c72af87f0ddf64432ad1ca51fa2332ecd34f091
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-14 20:16:10 -0400

    Fixed scope issue with printing array

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,5 @@
 public class SelectionSort {
-    void sort (int arr []) {
+    void sort(int arr[]) {
         // move boundary within array
         int n = arr.length;
         for (int i = 0; i < n; i++) {
@@ -7,16 +7,27 @@ public class SelectionSort {
             int min = i;
             // Find min within the array
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min]) {
+                if (arr[j] < arr[min])
                     min = j;
-                    // swap min with first element
-                    int temp = arr[min];
-                    arr[min] = arr[i];
-                    arr[i] = min;
-                }
-            }
+                // swap min with first element
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = min;
 
+            }
         }
+    }
+
+    // Print the given arrray
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + " ");
+            System.out.println();
+        }
+    }
+
+    {
 
     }
 }
