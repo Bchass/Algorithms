1201ac0e230c2f38daaf85ff77ea16321b30322d
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-15 17:58:40 -0400

    Find correct key

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,7 +1,16 @@
 public class InsertionSort {
-    void sort (int arr []){
-        for (int j = 1; j < arr.length; j++){
-            
+    void sort(int arr[]) {
+        for (int j = 1; j < arr.length; j++) {
+            // Find the correct key
+            int key = arr[j];
+            int i = j - 1;
+
+            while (i >= 0 && arr[i] > key) {
+                arr[i + 1] = arr[i];
+
+                i--;
+            }
+
         }
-    } 
+    }
 }
