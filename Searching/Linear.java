135ff87953b5f12ffdcdad4c06d29e25c07408d9
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-15 22:44:54 -0400

    Finished driver, needs work

--- Searching/Linear.java
+++ Searching/Linear.java
@@ -1,16 +1,26 @@
 package Searching;
 
-class Linear{
-    public static int search(int arr [], int x)
-    {   // Loop through to find the given target
+class Linear {
+    public static int search(int arr[], int x) { // Loop through to find the given target
         int n = arr.length;
-        for(int i = 0; n < arr.length; i++)
-        {
-            if(arr[i] == x)
-            {
+        for (int i = 0; n < arr.length; i++) {
+            if (arr[i] == x) {
                 return i;
             }
         }
-        return - 1;
+        return -1;
+    }
+
+    // Driver
+    public static void main(String[] args) {
+        int arr[] = { 3, 46, 78, 90, 23, 5, 67, 4 };
+        // The given target to find
+        int x = 4;
+
+        int result = search(arr, x);
+        if (result == -1)
+            ;
+        System.out.println("Target is not present");
+        System.out.println("Target is present at index " + result);
     }
 }
