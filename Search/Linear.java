e7e33c1330047b779008ab70037e7a11ed883804
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-16 17:10:56 -0400

    Fixed for loop to find required target

--- Search/Linear.java
+++ Search/Linear.java
@@ -3,7 +3,7 @@ package Search;
 class Linear {
     public static int search(int arr[], int x) { // Loop through to find the given target
         int n = arr.length;
-        for (int i = 0; n < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (arr[i] == x) {
                 return i;
             }
