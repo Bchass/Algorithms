f62f88f0818f742588b84bd1fd338af3a236b528
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-20 16:15:24 -0400

    Return based on position of index

--- Search/Binary.java
+++ Search/Binary.java
@@ -2,14 +2,20 @@ package Search;
 
 // Return index of x
 class Binary {
-    public static int Binary(int arr[], int t, int k, int x) {
+    int Binary(int arr[], int t, int k, int x) {
 
         if (k >= 1) {
             int middle = 1 + (k - 1) / 2;
             // If index is found in the middle
-            if (arr[middle] == x) {
+            if (arr[middle] == x) 
                 return middle;
+            // If element is smaller than index x, left side of array
+            if(arr[middle] > x){
+                return Binary(arr,1,middle,-1,x);
+                // Element right side of array
+                return Binary(arr,middle+1,k,x);
             }
+            return - 1;
         }
 
     }
