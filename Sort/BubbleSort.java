0e950145e86759964082bd23d5892509a2ef4af6
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-30 19:26:42 -0400

    Started

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,17 @@
+public class BubbleSort {
+    public void bubbleSort() {
+
+        int[] a = { 8, 4, 3, 5, 7 };
+        int size = a.length;
+
+        boolean sorted = false;
+        // break if inner loop was swapped
+        while (!sorted) {
+            for(int i = 0; size - 1; i++){
+                
+            }
+        }
+
+    }
+
+}
