2530093f771797c762bd0c1ab7ef69b659234ad0
Brandon Chasser <brandonchasser22@gmail.com>
2019-05-30 19:40:33 -0400

    Driver and array need to be fixed

--- Sort/BubbleSort.java
+++ Sort/BubbleSort.java
@@ -1,17 +1,33 @@
 public class BubbleSort {
-    public void bubbleSort() {
-
-        int[] a = { 8, 4, 3, 5, 7 };
-        int size = a.length;
+    public static void bubbleSort() {
 
         boolean sorted = false;
         // break if inner loop was swapped
         while (!sorted) {
-            for(int i = 0; size - 1; i++){
-                
+            for (int i = 0; i < size - 1; i++) {
+                sorted = true;
+            }
+            int j = i + 1;
+            if (a[i] > a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                sorted = false;
             }
         }
 
     }
 
+    {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+
+    // Driver
+    public static void main(String[]args){
+        int[] a = { 8, 4, 3, 5, 7 };
+        int size = a.length;
+    }
 }
