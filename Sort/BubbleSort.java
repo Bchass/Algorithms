33f78a60b217109c787857b711156520f63a18b9
Brandon Chasser <brandonchasser22@gmail.com>
2019-06-03 13:14:27 -0400

    Fixed break

--- Sort/BubbleSort.java
+++ Sort/BubbleSort.java
@@ -2,7 +2,7 @@ public class BubbleSort {
     void sort(int arr[], int n) {
 
         int i, j, temp;
-        //boolean swapped;
+        boolean swapped;
         for (i = 0; i < n - 1; i++) {
 
         }
@@ -16,11 +16,10 @@ public class BubbleSort {
                 swapped = true;
 
             }
+            // Break
+            if (swapped == false)
+                break;
         }
-        //TODO: 
-        // If nothing was switched then break
-        // if (swapped == false)
-        //     break;
 
     }
 
