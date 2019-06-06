6ab7564313d17b60468dfcc304dd79392d6f8fb3
Brandon Chasser <brandonchasser22@gmail.com>
2019-06-06 16:34:42 -0400

    Added tolerance and max

--- Clustering/K-Means.py
+++ Clustering/K-Means.py
@@ -1,4 +1,6 @@
 class kmeans:
 
-    def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+    def __init__(self, k = 3, tolerance = 0.0001, max_its = 1000):
+       self.k = k
+       self.tolerance = tolerance
+       self.max_its = max_its
