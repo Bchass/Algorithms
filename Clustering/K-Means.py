376b0647dfdc095dabce60cfd8dca28fdeb5c4b5
Brandon Chasser <brandonchasser22@gmail.com>
2019-06-06 16:37:51 -0400

    Updated amount of clusters

--- Clustering/K-Means.py
+++ Clustering/K-Means.py
@@ -1,6 +1,6 @@
 class kmeans:
 
-    def __init__(self, k = 3, tolerance = 0.0001, max_its = 1000):
+    def __init__(self, k = 4, tolerance = 0.0001, max_its = 1000):
        self.k = k
        self.tolerance = tolerance
        self.max_its = max_its
