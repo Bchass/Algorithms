5cd4244e5955ec63a4faa74015a6650529218e37
Brandon Chasser <brandonchasser22@gmail.com>
2019-06-17 19:17:38 -0400

    Created temp arrays

--- Sort/MergeSort.java
+++ Sort/MergeSort.java
@@ -1,3 +1,22 @@
-public class MergeSort{
-    
+public class MergeSort {
+    void sort(int arr[], int x, int m, int l) {
+
+        // Retreive size of arrays
+        int n1 = m - x + 1;
+        int n2 = l - m;
+
+
+        // Temp arrays
+        int M[] = new int[n1];
+        int L[] = new int[n2];
+
+        // Copy the data to temp arrays
+        for (int i = 0; i < n1; i++) {
+            M[i] = arr[x + 1];
+            for (int j = 0; j < n2; j++) {
+                L[j] = arr[m + 1 + j];
+            }
+        }
+
+    }
 }
