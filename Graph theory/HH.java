55f3ad60b748e0e7ab6c709f47cde3bf845a5dde
Brandon Chasser <brandonchasser22@gmail.com>
2019-06-27 19:52:48 -0400

    Remove all zeros through given dataset

--- Graph theory/HH.java
+++ Graph theory/HH.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class HH{
-
-    public static ArrayList<Integer> Havel(ArrayList<Integer> sequence){
+public class HH {
+    // Remove all zeros
+    public static ArrayList<Integer> Havel(ArrayList<Integer> sequence) {
+        Iterator itr = sequence.iterator();
+        while (itr.hasNext()) {
+            int x = (int) itr.next();
+            if (x == 0) {
+                itr.remove();
+            }
+        }
+        return sequence;
 
     }
 
