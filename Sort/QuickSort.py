d094a312c9fc07aa08873b41c213086d3aea427f
Brandon Chasser <brandonchasser22@gmail.com>
2020-01-01 14:01:01 -0500

    Completed

--- Sort/QuickSort.py
+++ Sort/QuickSort.py
@@ -1,6 +1,7 @@
-# Method partition
-def partition (start, end, arr):
-    i = (start-1)
+import array as arr
+# Partition
+def partition (arr, start, end):
+    i = ( start-1 )
     pivot = arr[end]
 
     for x in range (start, end):
@@ -11,3 +12,19 @@ def partition (start, end, arr):
 
             arr[i + 1], arr[end] = arr[end], arr[i + 1]
             return (i + 1)
+
+# Algorithm
+def QuickSort(arr, start, end):
+    if start < end:
+        par = partition (arr, start, end)
+
+        QuickSort(arr, start, par-1)
+        QuickSort(arr, end, par+1)
+
+#Driver
+arr = [6, 7, 9, 1, 4, 5]
+n = len(arr)
+QuickSort(arr, 0, n-1)
+print ("Sorted:")
+for i in range(n):
+    print( "%d" %arr[i])
