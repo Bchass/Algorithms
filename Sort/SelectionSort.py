3384bf3b67214e96a1832307fdf7cc8c696a7f45
Brandon Chasser <brandonchasser22@gmail.com>
2020-01-02 18:19:57 -0500

    Initial commit

---
+++ Sort/SelectionSort.py
@@ -0,0 +1,23 @@
+import array as arr
+
+def SelectionSort(arr):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Driver
+    def main():
+        SS = SelectionSort()
+        arr = [3, 78, 90, 22, 33, 41, 2]
+        SS.SelectionSort(arr)
+        print(arr)
