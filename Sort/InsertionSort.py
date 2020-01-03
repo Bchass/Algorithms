9bc818f8e054c2c4b8ab52a3ad4eb05c15d4610f
Brandon Chasser <brandonchasser22@gmail.com>
2020-01-02 19:39:43 -0500

    Fixed sort

--- Sort/InsertionSort.py
+++ Sort/InsertionSort.py
@@ -1,19 +1,20 @@
 import array as arr
 def InsertionSort(arr):
     n = len(arr)
-
     for i in range(n):
         key = arr[i]
         j = i - 1
 
-        while j >= 0 & arr[j] > key:
+        while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
+            j -= 1
             arr[j + 1] = key
 
 
 
 
 arr = [23, 45, 3, 5, 11]
+InsertionSort(arr)
 #Driver
 print("Sorted")
 for i in range(len(arr)):
