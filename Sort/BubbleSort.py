9eca918eebf5e4f9dcbf74182c35ad10861ecf86
Brandon Chasser <brandonchasser22@gmail.com>
2020-01-02 19:01:53 -0500

    cleanup

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -1,16 +1,15 @@
 import array as arr 
+def BubbleSort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+    
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 arr = [8, 4, 3, 5, 7]
-
+BubbleSort(arr)
+print("Sorted")
 for i in range(len(arr)):
-    swapped = False
-    for j in range(len(arr)):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], swapped
-
-            if swapped == False:
-                break
-
-            print("Sorted")
-            for i in range(len(arr)):
-                print("%d"%arr[i])
+    print("%d"%arr[i])
