ca17b7b8522c38c220f174f75a76f31b90494426
Brandon Chasser <brandonchasser22@gmail.com>
2020-01-04 14:40:34 -0500

    Initial commit

---
+++ Search/Binary.py
@@ -0,0 +1,25 @@
+import array as arr
+
+def Binary(arr, t, k, x):
+    if k >= 1:
+        middle = 1 + (k - 1) / 2
+
+        if arr[middle] == x:
+            return middle
+
+        if arr[middle] > x:
+            return Binary(arr, 1, middle - 1, x)
+
+            return Binary(arr, middle + 1, k, x)
+            return - 1
+
+
+# Driver
+arr = [2, 9, 80, 77, 36, 1, 5, 4]
+x = 9
+n = len(arr)
+result = Binary(arr, 0, n - 1, x)
+if result == 1:
+    print("Target is not present")
+else:
+    print("Target is present at index: " + result)
