5d38860b01318f3c920c8f8d96c2e2792793303e
Bchass <brandonchasser22@gmail.com>
2021-01-15 16:29:33 -0500

    Rework, wasn't sorting properly before

--- Sort/QuickSort/QuickSort.py
+++ Sort/QuickSort/QuickSort.py
@@ -1,30 +1,16 @@
-import array as arr
-# Partition
-def partition (arr, start, end):
-    i = ( start-1 )
-    pivot = arr[end]
+def QuickSort(list):
+    length = len(list)
+    if length <= 1:
+        return list
+    else:
+        pivot = list.pop()
+        high = []
+        low = []
 
-    for x in range (start, end):
-        if arr[x] <= pivot:
-            i = i +1
-            
-            arr[i], arr[x] = arr[x], arr[i]
-
-            arr[i + 1], arr[end] = arr[end], arr[i + 1]
-            return (i + 1)
-
-# Algorithm
-def QuickSort(arr, start, end):
-    if start < end:
-        par = partition (arr, start, end)
-
-        QuickSort(arr, start, par-1)
-        QuickSort(arr, end, par+1)
-
-#Driver
-arr = [6, 7, 9, 1, 4, 5]
-n = len(arr)
-QuickSort(arr, 0, n-1)
-print ("Sorted:")
-for i in range(n):
-    print( "%d" %arr[i])
+        for item in list:
+            if item > pivot:
+                high.append(item)
+            else:
+                low.append(item)
+        return QuickSort(low) + [pivot] + QuickSort(high)
+print(QuickSort([6,2,10,4,3,1]))
