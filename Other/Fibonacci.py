5bfb832d29958e155ec2e8f1d2c4ffa924c472cc
Bchass <brandonchasser22@gmail.com>
2021-01-17 12:08:46 -0500

    Refactor

--- Other/Fibonacci.py
+++ Other/Fibonacci.py
@@ -1,15 +1,7 @@
 
 def Fibonacci(n):
-    if  n <= 0:
-        print ("Wrong")
-        
-    elif n == 1:
-        return 0
-
-    elif n == 2:
-        return  1
-        
+    if n <= 1:
+        return n
     else:
         return Fibonacci(n-1) + Fibonacci (n-2)
-        
 print(Fibonacci(4))
